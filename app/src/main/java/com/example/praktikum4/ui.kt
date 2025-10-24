866a92eb882b5559da940a5cf7c824e1360e125f
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-24 08:01:25 +0700

    menambhkan leyout colum dan 3 text didalmnya yang menampilkan nama, nim dan alamat

--- app/src/main/java/com/example/praktikum4/ui.kt
+++ app/src/main/java/com/example/praktikum4/ui.kt
@@ -155,6 +155,27 @@ fun ActivitasPertama(modifier: Modifier){
                 )
 
                 Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 25.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text (
+                        stringResource(R.string.Nim),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Text (
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
             }
         }
 
